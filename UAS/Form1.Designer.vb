4b8745366fedca9b4328b2bdd92ba6c8bdca56c2
albiroby <garagauchiha11@gmail.com>
2024-02-17 17:49:42 +0700

    first commit

---
+++ UAS/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtkode = New System.Windows.Forms.TextBox()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtharga = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtjumlah = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txttotal = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtdata = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnsave = New System.Windows.Forms.Button()
+        Me.btnopen = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Kode Barang"
+        '
+        'txtkode
+        '
+        Me.txtkode.Location = New System.Drawing.Point(194, 38)
+        Me.txtkode.Name = "txtkode"
+        Me.txtkode.Size = New System.Drawing.Size(202, 22)
+        Me.txtkode.TabIndex = 1
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(194, 66)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(202, 22)
+        Me.txtnama.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nama Barang"
+        '
+        'txtharga
+        '
+        Me.txtharga.Location = New System.Drawing.Point(194, 94)
+        Me.txtharga.Name = "txtharga"
+        Me.txtharga.Size = New System.Drawing.Size(202, 22)
+        Me.txtharga.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(34, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Harga Satuan"
+        '
+        'txtjumlah
+        '
+        Me.txtjumlah.Location = New System.Drawing.Point(194, 122)
+        Me.txtjumlah.Name = "txtjumlah"
+        Me.txtjumlah.Size = New System.Drawing.Size(202, 22)
+        Me.txtjumlah.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(34, 125)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Jumlah"
+        '
+        'txttotal
+        '
+        Me.txttotal.Location = New System.Drawing.Point(194, 150)
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.Size = New System.Drawing.Size(202, 22)
+        Me.txttotal.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(34, 153)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 16)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Total Harga"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(37, 239)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 42)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Hitung"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(167, 239)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 42)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(301, 239)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(95, 42)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'txtdata
+        '
+        Me.txtdata.Location = New System.Drawing.Point(500, 69)
+        Me.txtdata.Multiline = True
+        Me.txtdata.Name = "txtdata"
+        Me.txtdata.Size = New System.Drawing.Size(322, 243)
+        Me.txtdata.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(511, 41)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(297, 20)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "TAMBAHAN LIST KODE BARANG"
+        '
+        'btnsave
+        '
+        Me.btnsave.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsave.Location = New System.Drawing.Point(500, 335)
+        Me.btnsave.Name = "btnsave"
+        Me.btnsave.Size = New System.Drawing.Size(110, 41)
+        Me.btnsave.TabIndex = 15
+        Me.btnsave.Text = "Save"
+        Me.btnsave.UseVisualStyleBackColor = True
+        '
+        'btnopen
+        '
+        Me.btnopen.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnopen.Location = New System.Drawing.Point(712, 335)
+        Me.btnopen.Name = "btnopen"
+        Me.btnopen.Size = New System.Drawing.Size(110, 41)
+        Me.btnopen.TabIndex = 17
+        Me.btnopen.Text = "Open"
+        Me.btnopen.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(892, 404)
+        Me.Controls.Add(Me.btnopen)
+        Me.Controls.Add(Me.btnsave)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtdata)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txttotal)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtjumlah)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtharga)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtkode)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "PROGRAM HITUNG BARANG"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtkode As TextBox
+    Friend WithEvents txtnama As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtharga As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtjumlah As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txttotal As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents txtdata As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnsave As Button
+    Friend WithEvents btnopen As Button
+End Class
---
+++ UAS/Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Private Function Hitung()
+        Dim Jumlah, Harga, Total As Integer
+        Jumlah = txtjumlah.Text
+        Harga = txtharga.Text
+        Total = Jumlah * Harga
+        txttotal.Text = Total
+        Return Total
+    End Function
+
+    Sub CekDataKosong()
+        If txtkode.Text = "" Then
+            MsgBox("Kode Tidak Boleh Kosong", 48, "Konfirmasi")
+            txtkode.Focus()
+        ElseIf txtnama.Text = "" Then
+            MsgBox("Nama Tidak Boleh Kosong", 48, "Konfirmasi")
+            txtnama.Focus()
+        ElseIf txtharga.Text = "" Then
+            MsgBox("Harga Tidak Boleh Kosong", 48, "Konfirmasi")
+            txtharga.Focus()
+        ElseIf txtjumlah.Text = "" Then
+            MsgBox("Jumlah Tidak Boleh Kosong", 48, "Konfirmasi")
+            txtjumlah.Focus()
+        Else
+            Call Hitung()
+        End If
+    End Sub
+
+    Sub Bersih()
+        txtkode.Clear() : txtnama.Clear()
+        txtharga.Clear() : txtjumlah.Clear()
+        txttotal.Text = "" : txtkode.Focus()
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call CekDataKosong()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Call Bersih()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If MsgBox("Program Akan Ditutup", 32 + 4, "Konfirmasi") = MsgBoxResult.Yes Then
+            Me.Close()
+        Else
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub btnsave_Click(sender As Object, e As EventArgs) Handles btnsave.Click
+        Dim MyWriter As StreamWriter
+        Dim dialog As SaveFileDialog
+        Dim Selected As Boolean
+        Dim filename As String
+
+        dialog = New SaveFileDialog()
+        Selected = dialog.ShowDialog
+        If Selected = True Then
+            filename = dialog.FileName
+            MyWriter = File.CreateText(filename)
+            MyWriter.WriteLine(txtdata.Text)
+            MyWriter.Close()
+        End If
+    End Sub
+
+    Private Sub btnopen_Click(sender As Object, e As EventArgs) Handles btnopen.Click
+        Dim MyReader As StreamReader
+        Dim dialog As OpenFileDialog
+        Dim selected As Boolean
+        Dim filename As String
+
+        dialog = New OpenFileDialog
+        selected = dialog.ShowDialog
+        If selected = True Then
+            filename = dialog.FileName
+            If File.Exists(filename) Then
+                MyReader = File.OpenText(filename)
+                txtdata.Text = MyReader.ReadToEnd
+                MyReader.Close()
+            End If
+        Else
+            MsgBox("Tidak Ada File Yang Dipilih")
+        End If
+    End Sub
+End Class
